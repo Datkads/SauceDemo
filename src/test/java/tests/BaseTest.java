91b33a6f30661dcff875d1a754f1fc9519fb064e
David <myerszbm@gmail.com>
2023-11-19 22:51:26 +0400

    feature/TMS 1 lesson 7 Object pages and Tests are done.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,21 @@
+package tests;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.ProductsPage;
+
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver chrome;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    CartPage cartPage;
 
     @BeforeMethod
     public void setup() {
@@ -14,6 +23,10 @@ public class BaseTest {
         chrome = new ChromeDriver();
         chrome.manage().window().maximize();
         chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        loginPage = new LoginPage(chrome);
+        productsPage = new ProductsPage(chrome);
+        cartPage = new CartPage(chrome);
     }
 
     @AfterMethod(alwaysRun = true)
