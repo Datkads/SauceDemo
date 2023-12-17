d30dbbe264f50d9584fbbd3ee84e2178bf72d5ef
David <myerszbm@gmail.com>
2023-12-17 13:46:47 +0400

    feature/TMS 1 lesson 15 Actions update: headless options added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,7 +3,9 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.*;
 import pages.CartPage;
 import pages.CheckoutPage;
@@ -25,12 +27,16 @@ public class BaseTest {
     public void setup(@Optional("chrome") String browser) {
         if(browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            chrome = new ChromeDriver();
-            chrome.manage().window().maximize();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            chrome = new ChromeDriver(options);
+            //chrome.manage().window().maximize();
         } else if (browser.equalsIgnoreCase("edge")) {
             WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("headless");
             chrome = new EdgeDriver();
-            chrome.manage().window().maximize();
+            //chrome.manage().window().maximize();
         }
         chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
